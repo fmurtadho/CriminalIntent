f5d183bb70998d8392e05327b42dc4165f6e8093
Faishal Murtadho <muhammad.faishal@myboost.id>
2019-08-18 06:27:03 +0700

    Rename activity_crime to make it generic, add abstract class & refactor CrimeActivity to use created abstract

--- app/src/main/java/com/fmurtadho/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/fmurtadho/android/criminalintent/CrimeActivity.java
@@ -1,25 +1,10 @@
 package com.fmurtadho.android.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
-
+public class CrimeActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container); // when you need to retrieve the CrimeFragment from FragmentManager
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment) // resouce ID of FrameLayout defined in activity_crime.xml & where to put it
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
